f40abe88613fe40f34c2ca7f9a911b68e08fb28c
Daniele Cruciani <daniele@smartango.com>
2025-01-27 16:31:33 +0100

    serving static contents

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@
 // using axum
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
-use apimanager_service::assets::static_files::INDEX_HTML;
+use apimanager_service::assets::static_files::{INDEX_BUNDLE_JS, INDEX_BUNDLE_JS_MAP, INDEX_HTML};
 
 #[tokio::main]
 async fn main() {
     //let app ("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/index.html", get(|| async { INDEX_HTML }))
+    .route("/index.bundle.js", get(|| async { INDEX_BUNDLE_JS}))
+    .route("/index.bundle.js.map", get(|| async { INDEX_BUNDLE_JS_MAP}));
 
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
     println!("{:?}", INDEX_HTML);
     println!("Hello, world!");
 }
